bae58ca2195d70e4dcd02d610d9c7137c3940e34
pexcn <pexcn97@gmail.com>
2025-01-28 11:31:08 +0800

    first version

--- build.sh
+++ build.sh
@@ -1,21 +1,18 @@
-#!/bin/sh
-# shellcheck disable=SC1090,SC2086,SC2155,SC2103,SC2164,SC3043
-
-CUR_DIR=$(dirname "$(readlink -f "$0")")
+#!/bin/bash
+# shellcheck disable=SC1090,SC2086,SC2164,SC2103,SC2155
 
 prepare_env() {
-  . profile/${BUILD_PROFILE}
-  mkdir build
+  CUR_DIR=$(dirname "$(readlink -f "$0")")
+  . config/$BUILD_CONFIG.conf
+  mkdir -p build dl
 }
 
 setup_clang() {
   cd build
 
   local clang_pack="$(basename $CLANG_URL)"
-  mkdir clang
-  wget $CLANG_URL
-  tar -C clang/ -zxf $clang_pack
-  rm $clang_pack
+  [ -f $CUR_DIR/dl/$clang_pack ] || wget -q $CLANG_URL -P $CUR_DIR/dl
+  mkdir clang && tar -C clang/ -zxf $CUR_DIR/dl/$clang_pack
 
   cd -
 }
@@ -24,26 +21,43 @@ setup_gcc() {
   cd build
 
   local gcc_pack="$(basename $GCC_URL)"
-  mkdir gcc
-  wget $GCC_URL
-  tar -C gcc/ -zxvf $gcc_pack
-  rm $gcc_pack
+  [ -f $CUR_DIR/dl/$gcc_pack ] || wget -q $GCC_URL -P $CUR_DIR/dl
+  mkdir gcc && tar -C gcc/ -zxf $CUR_DIR/dl/$gcc_pack
 
   cd -
 }
 
 get_sources() {
-  git clone $KERNEL_SOURCE build/kernel
+  [ -d build/kernel/.git ] || git clone $KERNEL_SOURCE build/kernel
   cd build/kernel
-  
+  git diff --quiet HEAD || git reset --hard HEAD
+
   # checkout version
   git checkout $KERNEL_VERSION
 
   # remove `-dirty` of version
   sed -i 's/ -dirty//g' scripts/setlocalversion
 
+  cd -
+}
+
+add_kernelsu() {
+  cd build/kernel
+
   # integrate kernelsu-next
   curl -sSL "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s $KSU_NEXT_VERSION
+
+  # update kernel config
+  cat <<-EOF >>arch/arm64/configs/${KERNEL_CONFIG%% *}
+	CONFIG_MODULES=y
+	CONFIG_KPROBES=y
+	CONFIG_HAVE_KPROBES=y
+	CONFIG_KPROBE_EVENTS=y
+	EOF
+
+  # re-generate kernel config
+  make O=out ARCH=arm64 $KERNEL_CONFIG savedefconfig
+  cp -f out/defconfig arch/arm64/configs/${KERNEL_CONFIG%% *}
 
   cd -
 }
@@ -51,35 +65,34 @@ get_sources() {
 build_kernel() {
   cd build/kernel
 
+  export PATH=$CUR_DIR/build/clang/bin:$PATH
+  export KBUILD_BUILD_USER=${GITHUB_REPOSITORY_OWNER:-pexcn}
+  export KBUILD_BUILD_HOST=buildbot
   export KBUILD_COMPILER_STRING="$(clang --version | head -1 | sed 's/ (https.*//')"
-  export KBUILD_BUILD_HOST=GitHub
-  export KBUILD_BUILD_USER=pexcn
-  export BRAND_SHOW_FLAG=oneplus
+  export ARCH=arm64 # really need?
+  export SUBARCH=arm64 # really need?
 
-  export ARCH=arm64
-  export SUBARCH=arm64
-  make -j$(($(nproc) + 1)) vendor/kona-perf_defconfig vendor/debugfs.config \
-    ARCH=arm64 \
-    O=out \
-    LD=ld.lld \
-    LLVM=1 \
-    LLVM_IAS=1 \
-    || exit 1
-
-  echo $CUR_DIR
+  MAKE_FLAGS=(
+    O=out
+    ARCH=arm64
+    CLANG_TRIPLE=aarch64-linux-gnu-
+    CROSS_COMPILE=aarch64-linux-android-
+    CC="ccache clang"
+    LD=ld.lld
+    LLVM=1
+    LLVM_IAS=1
+  )
+  # select kernel config
+  make "${MAKE_FLAGS[@]}" $KERNEL_CONFIG
+  # compile kernel
+  make "${MAKE_FLAGS[@]}" -j$(($(nproc) + 1)) || exit 1
 
   cd -
 }
-
-#package_output() {
-#  local output_dir="build/kernel/out"
-#  local tarball="${BUILD_PROFILE}.tar.gz"
-#  tar -zcvf $tarball -C $output_dir $(ls $output_dir -1)
-#}
 
 prepare_env
 setup_clang
 setup_gcc
 get_sources
+add_kernelsu
 build_kernel
-
