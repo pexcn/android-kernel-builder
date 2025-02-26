9f9ed6d8f1cf89f3ef3dfd1d171b55d9e55671fa
pexcn <pexcn97@gmail.com>
2025-02-26 22:08:48 +0800

    [release] update KernelSU-Next to 1.0.5

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ add_kernelsu() {
   cd build/kernel
 
   # integrate kernelsu-next
-  curl -sSL "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.4
+  curl -sSL "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s v1.0.5
 
   # prepare .config
   make "${MAKE_FLAGS[@]}" $KERNEL_CONFIG
