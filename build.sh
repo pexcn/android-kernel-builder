c05098a8110ebd12b267f41acefd83e72b7db96b
pexcn <pexcn97@gmail.com>
2025-04-29 17:34:19 +0800

    disable CONFIG_ZRAM_WRITEBACK

--- build.sh
+++ build.sh
@@ -135,6 +135,7 @@ optimize_config() {
   # disable unused features
   scripts/config --file out/.config \
     --disable CONFIG_KSU_SUSFS \
+    --disable CONFIG_ZRAM_WRITEBACK \
     --disable CONFIG_CAN \
     --disable CONFIG_MMC \
     --disable CONFIG_FTRACE \
